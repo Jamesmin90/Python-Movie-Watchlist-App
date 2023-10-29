8bb93d66e363398f53d9fafd0adb7db1e15aef1f
James Min <54721700+Jamesmin90@users.noreply.github.com>
2023-10-29 16:41:05 +0000

    postgres

--- database.py
+++ database.py
@@ -1,48 +1,79 @@
+import os
 import datetime
-import sqlite3
+import psycopg2
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
+    id SERIAL PRIMARY KEY,
     title TEXT,
-    release_timestamp REAL,
-    watched INTEGER
+    release_timestamp REAL
 );"""
 
-INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
-SELECT_ALL_MOVIES = "SELECT * FROM movies;"
-SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
-SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
-SELECT_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY
+);"""
 
-connection = sqlite3.connect("data.db")
+CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched (
+    user_username TEXT,
+    movie_id INTEGER,
+    FOREIGN KEY(user_username) REFERENCES users(username),
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
+);"""
+
+INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp) VALUES (%s, %s);"
+INSERT_USER = "INSERT INTO users (username) VALUES (%s);"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = %s;"
+SELECT_ALL_MOVIES = "SELECT * FROM movies;"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > %s;"
+SELECT_WATCHED_MOVIES = "SELECT * FROM watched WHERE watcher_name = %s;"
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_name, movie_id) VALUES (%s, %s);"
+
+
+
+connection = psycopg2.connect(os.environ["DATABASE_URL"])
 
 def create_tables():
     with connection:
-        connection.execute(CREATE_MOVIES_TABLE)
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_MOVIES_TABLE)
+            cursor.execute(CREATE_USERS_TABLE)
+            cursor.execute(CREATE_WATCHED_TABLE)
 
 
 def add_movie(title, release_timestamp):
     with connection:
-        connection.execute(INSERT_MOVIE, (title, release_timestamp))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_MOVIE, (title, release_timestamp))
 
 
 def get_movies(upcoming=False):
     with connection:
-        cursor = connection.cursor
-        if upcoming:
-            timestamp = datetime.datetime.today().timestamps()
-            cursor.execute(SELECT_UPCOMING_MOVIES, (timestamp,))
-        else:
-            cursor.execute(SELECT_ALL_MOVIES)
-        return cursor.fetchall()
+        with connection.cursor() as cursor:
+            if upcoming:
+                timestamp = datetime.datetime.today().timestamps()
+                cursor.execute(SELECT_UPCOMING_MOVIES, (timestamp,))
+            else:
+                cursor.execute(SELECT_ALL_MOVIES)
+            return cursor.fetchall()
 
 
-def watch_movie(movie_title):
+def watch_movie(username, movie_id):
     with connection:
-        connection.execute(SELECT_WATCHED_MOVIES, (movie_title,))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
 
 
-def get_watched_movies(movie_title):
+def get_watched_movies(username):
     with connection:
-        cursor = connection.cursor()
-        cursor.execute(SELECT_WATCHED_MOVIES, (movie_title,))
-        return cursor
+        with connection.cursor() as cursor:
+            cursor = connection.cursor()
+            cursor.execute(SELECT_WATCHED_MOVIES, (username,))
+            return cursor.fetchall()
+
+def add_user(username):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_USER, (username,))
